f82f37fc7ceae7271df90888ea00708e74fe6397
LeeTouKo <touko.lee01@gmail.com>
2023-01-06 20:45:53 -0600

    names appear under Numbers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import { useState } from 'react'
 
+const Person = (props) => {
+  return(
+    <li>{props.id} {props.name}</li>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', id: 1}
   ]) 
   const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const Person = {
+      name: newName,
+      id: persons.length + 1
+    }
+    setPersons(persons.concat(Person))
+    setNewName('')
+  }
+
+  const handlePersonsChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input 
+            value={newName}
+            onChange={handlePersonsChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map(person => 
+          <Person key={person.id} name={person.name} />
+        )}
+      </div>
     </div>
   )
 }
