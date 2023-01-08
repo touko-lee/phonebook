ed4a416b4c88af99f75a446b8db6daf6e96f1a0c
LeeTouKo <touko.lee01@gmail.com>
2023-01-07 21:36:46 -0600

    added function to filter through names if the names contains the filter

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ const Person = (props) => {
     <li>{props.id} {props.name} {props.number}</li>
   )
 }
-
+// part 5
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 1}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -39,9 +44,20 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input
+          value={newFilter}
+          onChange={handleFilterChange}
+          />
+      </div>
+      <h2>Add new person</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -61,7 +77,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => 
+        {persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase())).map(person => 
           <Person key={person.id} name={person.name} number={person.number} />
         )}
       </div>
